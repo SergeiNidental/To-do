bde2811d432ef3085d326fb27731443ac26f92bd
SergeiNidental <sergeinidental@gmail.com>
2023-10-18 18:34:33 +0400

    Remade file structure

--- src_react/scripts/app.jsx
+++ src_react/scripts/app.jsx
@@ -1,6 +1,6 @@
 import React, { StrictMode } from "react";
-import Header from "./header";
-import Addform from "./addform";
+import Header from "./header/header";
+import Addform from "./addform/addform";
 
 export default function App() {
   return (
