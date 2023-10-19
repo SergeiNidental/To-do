d9603137608c983718c5c7b70e31ab7e8b92c1a8
SergeiNidental <sergeinidental@gmail.com>
2023-10-19 15:50:58 +0400

    OnChange for Input

--- src_react/scripts/addform/form.jsx
+++ src_react/scripts/addform/form.jsx
@@ -1,15 +1,26 @@
 import React, { StrictMode } from "react";
+import { useState } from "react";
 
 export function Form(props) {
-  return <form className={props.className}>{props.children}</form>;
+  const [Ulist, setUlist] = useState([]);
+
+  return <form className={props.className} {...Ulist, setUlist}>{props.children}</form>;
 }
 
 export function Input(props) {
+  const [inputState, setInputState] = useState("");
+
+  function handleOnChange(e) {
+    setInputState(e.target.value);
+  }
+
   return (
     <input
       type={props.type}
       className={props.className}
       placeholder={props.placeholder}
+      onChange={handleOnChange}
+      value={inputState}
     />
   );
 }
